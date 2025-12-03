df2536d6d3a4fc46987dd4877093147a07e02b4d
natalia <napo004@student.kristiania.no>
2025-12-03 14:50:49 +0100

    edits i yaml filer og vi har nå koblet til databasen, springboot fungerer! :))))))

--- src/main/resources/db/migration/V001__initial_structure.sql
+++ src/main/resources/db/migration/V001__initial_structure.sql
@@ -3,7 +3,17 @@ CREATE TABLE customer(
     name VARCHAR (150),
     phone_number VARCHAR (30),
     email VARCHAR (75)
-)
+);
+
+CREATE TABLE address(
+    address_id BIGINT PRIMARY KEY,
+    customer_id BIGINT REFERENCES customer(customer_id),
+    apartment_number VARCHAR(15),
+    address VARCHAR (150),
+    zip_code VARCHAR (30),
+    city VARCHAR (40),
+    country VARCHAR (50)
+);
 
 CREATE TABLE product(
     product_id BIGINT PRIMARY KEY ,
@@ -12,44 +22,28 @@ CREATE TABLE product(
     price DECIMAL,
     status VARCHAR (25),
     quantity INT
-)
+);
 
-CREATE TABLE order (
+CREATE TABLE customer_order (
     order_id BIGINT PRIMARY KEY ,
-    customer_id BIGINT,
-    address_id BIGINT,
+    customer_id BIGINT REFERENCES customer(customer_id),
+    address_id BIGINT REFERENCES address(address_id),
     order_date DATE,
     total_sum INT,
     shipping_charge INT,
-    status VARCHAR (30),
-    FOREIGN KEY (customer_id) references customer(customer_id),
-    FOREIGN KEY (address_id) references customer_address(address_id)
-)
-
-CREATE TABLE address(
-    address_id BIGINT PRIMARY KEY,
-    customer_id BIGINT,
-    apartment_number VARCHAR(15),
-    address VARCHAR (150),
-    zip_code VARCHAR (30),
-    city VARCHAR (40),
-    country VARCHAR (50),
-    FOREIGN KEY (customer_id) references customer(customer_id)
-)
+    status VARCHAR (30)
+);
 
 CREATE TABLE order_product(
     order_product_id BIGINT PRIMARY KEY,
-    order_id BIGINT,
-    product_id BIGINT,
+    order_id BIGINT REFERENCES customer_order(order_id),
+    product_id BIGINT REFERENCES product(product_id),
     quantity INT,
-    unit_price INT,
-    FOREIGN KEY (order_id) references order(order_id),
-    FOREIGN KEY (product_id) references product(product_id)
-)
-
+    unit_price INT
+);
 
 create sequence customer_seq increment by 1 start with 1;
 create sequence order_seq increment by 1 start with 1;
 create sequence address_seq increment by 1 start with 1;
-create sequence address_seq increment by 1 start with 1;
+create sequence product_seq increment by 1 start with 1;
 create sequence order_product_seq increment by 1 start with 1;
