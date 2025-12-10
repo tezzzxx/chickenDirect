8a09a91ed11910dd554a0a8f86b946a2cf5e7ae8
natalia <napo004@student.kristiania.no>
2025-12-10 16:02:58 +0100

    Fikset en liten errorMessage slik at vi kan se det i postman under message.

---
+++ src/main/java/org/example/chickendirect/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.example.chickendirect;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(
+            ResponseStatusException ex, HttpServletRequest request){
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", new Date());
+        error.put("status", ex.getStatusCode().value());
+        error.put("error", ex.getStatusCode().toString());
+        error.put("message", ex.getReason());
+        error.put("path", request.getRequestURI());
+
+        return new ResponseEntity<>(error, ex.getStatusCode());
+    }
+}
+
+
+
