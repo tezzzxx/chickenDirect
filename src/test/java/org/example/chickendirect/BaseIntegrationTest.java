64a698a4eb578193d21e953a40044381b422cbc9
natalia <napo004@student.kristiania.no>
2025-12-17 15:05:34 +0100

    Start på tester

---
+++ src/test/java/org/example/chickendirect/BaseIntegrationTest.java
@@ -0,0 +1,5 @@
+package org.example.chickendirect;
+
+public class BaseIntegrationTest {
+
+}
---
+++ src/test/java/org/example/chickendirect/integration/repo/ProductRepoIntegrationTest.java
@@ -0,0 +1,15 @@
+package org.example.chickendirect.integration.repo;
+
+import org.example.chickendirect.BaseIntegrationTest;
+import org.example.chickendirect.repos.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ProductRepoIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private ProductRepo productRepo
+
+}
+
+
